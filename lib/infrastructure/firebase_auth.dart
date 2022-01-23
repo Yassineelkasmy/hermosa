2667864e2f2f08ea97f1780824cb22974f6b8860
yassineelkasmy <yassineelkasmy@gmail.com>
2022-01-23 13:31:14 +0000

    Add firebase authentication implementation

---
+++ lib/infrastructure/firebase_auth.dart
@@ -0,0 +1,61 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hermosa/domain/auth/auth_failure.dart';
+import 'package:hermosa/domain/auth/i_auth_facade.dart';
+import 'package:hermosa/domain/auth/user.dart';
+
+@Injectable(as: IAuthFacade)
+class FireBaseAuth implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FireBaseAuth(this._firebaseAuth, this._googleSignIn);
+
+  @override
+  Future<Option<User>> getSignedUser() async {
+    final user = _firebaseAuth.currentUser;
+    if (user == null) {
+      return none();
+    } else {
+      return optionOf(
+        User(
+          uid: user.uid,
+          email: user.email!,
+          displayName: user.displayName!,
+          photoURL: user.photoURL!,
+          accessToken: await user.getIdToken(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+
+      final googleAuthentication = await googleUser.authentication;
+
+      final authCredential = GoogleAuthProvider.credential(
+        idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken,
+      );
+
+      await _firebaseAuth.signInWithCredential(authCredential);
+      return right(unit);
+    } on FirebaseAuthException catch (_) {
+      return left(const AuthFailure.serverError());
+    }
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 
-void main() {
+import 'injection.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  configureInjection(Environment.prod);
   runApp(const MyApp());
 }
 
