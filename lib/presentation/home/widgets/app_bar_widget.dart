e0f9813b1b062eeb00d9578db14cf7d0b2e660a8
yassineelkasmy <yassineelkasmy@gmail.com>
2022-01-25 23:41:41 +0000

    Modify the custom appBar : Add user profile image

--- lib/presentation/home/widgets/app_bar_widget.dart
+++ lib/presentation/home/widgets/app_bar_widget.dart
@@ -1,40 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:hermosa/domain/auth/user.dart';
-import 'package:hermosa/presentation/utils/add_space.dart';
 
 AppBar buildAppBar(User user) {
   return AppBar(
-    centerTitle: true,
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: 25,
-          height: 25,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-                image: NetworkImage(
-                  user.photoURL!,
-                ),
-                fit: BoxFit.fill),
-          ),
-        ),
-        addHorizontalSpace(10),
-        Text(
-          user.displayName,
-        ),
-      ],
-    ),
     leading: IconButton(
       onPressed: () {},
-      icon: const Icon(
-        Icons.menu,
-        color: Colors.black,
+      icon: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+              user.photoURL,
+            ),
+            fit: BoxFit.fitWidth,
+          ),
+        ),
       ),
     ),
+    backgroundColor: Colors.white,
     elevation: 0,
     actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+      ),
       Padding(
         padding: const EdgeInsets.only(right: 10),
         child: IconButton(
